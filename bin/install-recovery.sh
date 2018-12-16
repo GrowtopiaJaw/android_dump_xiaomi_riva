525405364e7e7a0b0c6e3d4ee112e7d1c4a55ae1
Growtopia Jaw <growtopiajaw@gmail.com>
2018-12-16 09:21:11 +0000

    MIUI10 - OPM1.171019.026 - 8.12.13

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:38624590:752c9d2a90b8ba3d9b72bc7f5f8691a444bc552e; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:34417994:21056a275ebe29692d04504a0bb54c51ef8f1863 EMMC:/dev/block/bootdevice/by-name/recovery 752c9d2a90b8ba3d9b72bc7f5f8691a444bc552e 38624590 21056a275ebe29692d04504a0bb54c51ef8f1863:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:38626638:b79b38c463a0fa6926a96f0553674295692ed0e5; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:34420042:a25c416e882f5626447ae0e77da4f2f01275558c EMMC:/dev/block/bootdevice/by-name/recovery b79b38c463a0fa6926a96f0553674295692ed0e5 38626638 a25c416e882f5626447ae0e77da4f2f01275558c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
